9e91e4de5a70766365adfcfe3cca3032b3dfe485
GeovaneVLG <geovanevinicius2012@hotmail.com>
2021-11-10 20:38:19 -0300

    Arquivo para criação do BD

--- src/cadastro-cliente.php
+++ src/cadastro-cliente.php
@@ -2,6 +2,19 @@
 
 if(isset($_POST['submit'])){
     
+    include_once('config.php');
+
+    $nome = $_POST['nomeCliente'];
+    $email = $_POST['emailCliente'];
+    $cpfCliente = $_POST['cpfCliente'];
+    $dataNasCliente = $_POST['dataNasCliente'];
+    $telCliente = $_POST['telCliente'];
+    $celCliente = $_POST['celCliente'];
+    $usuarioCliente = $_POST['usuarioCliente'];
+    $senhaCliente = $_POST['senhaCliente'];
+
+    $result = mysqli_query($conexao,"INSERT INTO cliente()");
+
 }
 
 
